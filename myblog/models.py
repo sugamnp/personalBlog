61db4e6cdeb715deff1401aba92069160d943482
Sugam Neupan <sugamneupane227@gmail.com>
2021-12-11 16:50:00 +0545

    images made static and removed unnecessary divs

--- myblog/models.py
+++ myblog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+class Post(models.Model):
+    blog_id = models.AutoField
+    blog_writer = models.CharField(max_length=15, default="")
+    blog_title = models.CharField(max_length=100, default ="")
+    blog_body = models.CharField(max_length=1000000, default ="")
+    blog_created_at = models.DateTimeField(default=datetime.now, blank=True)
+    blog_image = models.ImageField(upload_to='{%static "images/posts" %}', default ="")
+
+    def __str__(self):
+        return self.blog_title
