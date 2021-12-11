e9396322f3a51ba0bc8a0edd2492b4078ff67569
Sugam Neupan <sugamneupane227@gmail.com>
2021-12-11 15:51:08 +0545

    template change and added static files

--- myblog/urls.py
+++ myblog/urls.py
@@ -1,3 +1,10 @@
+from django.urls import path
+
 from . import views
 urlpatterns = [
+    path('', views.home, name='home'),
+    path('blogs', views.blogs, name='blogs'),
+    path('aboutus', views.blogs, name='aboutus'),
+    path('contact', views.blogs, name='contact'),
+
 ]
--- myblog/views.py
+++ myblog/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    return render(request, 'base.html')
+
+def blogs(request):
+    return render(request, 'myblog/blogdetails.html')
+
+def aboutus(request):
+    return render(request, 'myblog/blogdetails.html')
+
+def contact(request):
+    return render(request, 'myblog/contact.html')
